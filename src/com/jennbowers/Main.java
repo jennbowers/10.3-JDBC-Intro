8e35263e358184b158cea8303614ef8fdf7f4656
Jenn Bowers <jenniferbowers03@gmail.com>
2017-07-26 10:01:57 -0400

    Drop table if does exist, create table if doesn't exist, insert information into table, find all information and print it out. Done with create read operations.

--- src/com/jennbowers/Main.java
+++ src/com/jennbowers/Main.java
@@ -12,7 +12,24 @@ public class Main {
             Statement statement = connection.createStatement();
 //            cleans up after itself
             statement.executeUpdate("DROP TABLE IF EXISTS stats");
-            statement.executeUpdate("CREATE TABLE stats (id INTEGER PRIMARY, name STRING, wins INTEGER, losses INTEGER)");
+//            creates table with specified columns
+            statement.executeUpdate("CREATE TABLE stats (id INTEGER PRIMARY KEY, name STRING, wins INTEGER, losses INTEGER)");
+//            insert values into table
+            statement.executeUpdate("INSERT INTO stats (name, wins, losses) VALUES ('Jenn', 10, 2)");
+//            select and get all from table and store in a result set... means you can look through and get info back one row at a time
+            ResultSet rs = statement.executeQuery("SELECT * FROM stats");
+//            iterates through results one row at a time while there is a next row
+            while(rs.next()) {
+//                assigned what you are getting back for each column to a variable
+              String name = rs.getString("name");
+              int wins = rs.getInt("wins");
+              int losses = rs.getInt("losses");
+//              prints variables for name, wins and losses
+              System.out.printf("%s %s %s", name, wins, losses);
+            }
+
+
+
         } catch (SQLException ex) {
             System.out.println("Something went wrong with your DB connection.");
             ex.printStackTrace();
