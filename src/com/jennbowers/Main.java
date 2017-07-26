aae095cd87c351339e40a22aec3dc44e840d6f4c
Jenn Bowers <jenniferbowers03@gmail.com>
2017-07-26 09:39:55 -0400

    Boilerplate to create and connect to a sqlite db

--- src/com/jennbowers/Main.java
+++ src/com/jennbowers/Main.java
@@ -1,8 +1,19 @@
 package com.jennbowers;
 
+import java.sql.*;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws ClassNotFoundException{
+        Class.forName("org.sqlite.JDBC");
+
+//        opens a connection to a database if it already exists or creates a database called stats and connecting to it
+        try (Connection connection = DriverManager.getConnection("jdbc:sqlite:stats.db")) {
+
+        } catch (SQLException ex) {
+            System.out.println("Something went wrong with your DB connection.");
+            ex.printStackTrace();
+        }
+
     }
 }
