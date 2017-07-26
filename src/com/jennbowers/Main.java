414ed9d10bc14573e80ab5bd650e0bd100f0b3f5
Jenn Bowers <jenniferbowers03@gmail.com>
2017-07-26 09:46:20 -0400

    Create and drop table now possible

--- src/com/jennbowers/Main.java
+++ src/com/jennbowers/Main.java
@@ -9,7 +9,10 @@ public class Main {
 
 //        opens a connection to a database if it already exists or creates a database called stats and connecting to it
         try (Connection connection = DriverManager.getConnection("jdbc:sqlite:stats.db")) {
-
+            Statement statement = connection.createStatement();
+//            cleans up after itself
+            statement.executeUpdate("DROP TABLE IF EXISTS stats");
+            statement.executeUpdate("CREATE TABLE stats (id INTEGER PRIMARY, name STRING, wins INTEGER, losses INTEGER)");
         } catch (SQLException ex) {
             System.out.println("Something went wrong with your DB connection.");
             ex.printStackTrace();
